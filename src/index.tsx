36e8d16a8a132cb5716612985b6f0528128576b7
Claire Lu <appsfunlu@gmail.com>
2022-02-07 22:06:39 -0800

    functional component

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+
+
+// Render this document with the component
+ReactDOM.render(<App />, document.getElementById('root'))
